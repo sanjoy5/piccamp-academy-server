aa9ce628db6e84175077861d21e599e785d978b0
Sanjoy Sarker <sarkerd52@gmail.com>
2023-06-08 15:21:42 +0600

    user save in database done

--- index.js
+++ index.js
@@ -3,11 +3,62 @@ const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 // middleware 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nuk8vmz.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const usersCollection = client.db('piccampDB').collection('users')
+
+
+        // Users Collections 
+
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existsUser = await usersCollection.findOne(query)
+            if (existsUser) {
+                return res.send({ message: 'user alerady exists' })
+            }
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 
